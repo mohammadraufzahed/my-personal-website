1de85c3dd65d3dd9d405a97cb651f458cc4a7ea1
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2020-05-23 16:45:51 +0430

    Change everything

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: `Mohammad Raufzahed` });
+});
+
+module.exports = router;
