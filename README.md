060922ff4a08357e38637ced9684da8992e02712
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2020-07-12 14:26:00 +0430

    Initial v2 of my website

--- README.md
+++
@@ -1,7 +0,0 @@
-# mohammadraufzahed.github.io
-My personal website
-# What i use to make this website?
-Nodejs<br>
-Expressjs<br>
-Pug<br>
-Liara pass<br>
--- public/javascripts/mail.js
+++
@@ -1,6 +0,0 @@
-function sendMail(){
-    let subject = document.getElementById('subject').value;
-    let mail = document.getElementById('mail'.value);
-    let message = document.getElementById('message').value;
-    window.open(`mailto:${mail}?subject=${subject}&body=${message}`);
-}
