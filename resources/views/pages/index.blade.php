679367645825863a4284e6b8f03c6b9a36b663eb
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2023-08-24 10:26:30 +0330

    Migrated the contact me

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -13,4 +13,14 @@
     <x-skills />
     <x-services />
     <x-portfolio />
+    <div id="contact_me" class="w-full h-max flex flex-col items-center justify-start gap-[42px] py-8">
+        <h2
+            class="w-full font-black text-primary text-center text-xl sm:text-2xl leading-header md:text-3xl md:leading-header  lg:text-5xl lg:leading-header">
+            بیا در مورد پروژت صحبت کنیم!
+        </h2>
+        <a href="mailto:mohammadraufzahedlink@proton.me"
+            class="font-black cursor-pointer text-base text-white px-8 py-4 bg-primary rounded-box">
+            تماس با من
+        </a>
+    </div>
 </x-layouts.app>
