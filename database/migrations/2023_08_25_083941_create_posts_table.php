0661e6074428bb0a354b04b50e24a10220ac7302
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2023-08-25 15:11:02 +0330

    Added the Post,Tag models and improved the user model

---
+++ database/migrations/2023_08_25_083941_create_posts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id()->index();
+            $table->text('title')->nullable(false)->index();
+            $table->text('slug')->unique(true)->nullable(false)->index();
+            $table->text('content')->nullable(false);
+            $table->foreignIdFor(User::class, 'user_id')->index();
+            $table->text('summary')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
---
+++ database/migrations/2023_08_25_100241_create_tags_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Models\Post;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tags', function (Blueprint $table) {
+            $table->id()->index();
+            $table->text('content')->index();
+            $table->foreignIdFor(Post::class, 'post_id')->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tags');
+    }
+};
